758155aed4807378854d3908267cf5c00dc68dfd
dom20202123 <dominiquecurry2006@gmail.com>
2025-04-25 21:36:54 -0500

    test

---
+++ television.py
@@ -0,0 +1,45 @@
+class Television():
+        MIN_VOLUME = 0
+        MAX_VOLUME = 2
+        MIN_CHANNEL = 0
+        MAX_CHANNEL = 3
+        def __init__(self):
+            self.__status = False
+            self.__muted = False
+            self.__volume = Television.MIN_VOLUME
+            self.__channel = Television.MIN_CHANNEL
+
+        def power(self):
+            self.__status = True
+
+        def mute(self):
+            self.__muted = True
+
+        def channel_up(self):
+            if self.__status == True:
+                self.__channel += 1
+                if  self.__channel > Television.MAX_CHANNEL:
+                    self.__channel = Television.MIN_CHANNEL
+
+        def channel_down(self):
+            if self.__status == True:
+                self.__channel -= 1
+                if  self.__channel < Television.MIN_CHANNEL:
+                    self.__channel = Television.MAX_CHANNEL
+
+        def volume_up(self):
+            if self.__status == True:
+                self.__muted = False
+                self.__volume += 1
+                if self.__volume >= Television.MAX_VOLUME:
+                    self.__volume = Television.MAX_VOLUME
+
+        def volume_down(self):
+            if self.__status == True:
+                self.__muted = False
+                self.__volume -= 1
+                if self.__volume <= Television.MIN_VOLUME:
+                    self.__volume = Television.MIN_VOLUME
+
+        def __str__(self):
+            print(f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}')
